98b44cdb40ce61eb7a68b22117b7361ed7541884
Astha Samaddar <95849912+Asttttha@users.noreply.github.com>
2024-07-25 18:22:50 +0530

    academics

--- src/component/Academics.js
+++ src/component/Academics.js
@@ -12,7 +12,7 @@ const Academics = () => {
         setCurrentPage(change)
     }
     return (
-        <div className='flex items-center flex-col p-5 gap-5 md:p-10  md:gap-10'>
+        <div className='flex items-center flex-col p-5 gap-5 md:p-10 pt-24 md:pt-24 md:gap-10'>
             <p className='text-4xl font-bold text-gray-700'>Academics</p>
             <div className='flex flex-col gap-2 md:flex-row md:gap-10'>
                 <button className={`px-8 py-2 font-semibold rounded hover:bg-blue-900/100 hover:text-white transition duration-300 ${currentPage === "Curriculum"?("bg-blue-900/100 text-white"):("bg-white text-black")}`} onClick={() => handlerPage("Curriculum")}>Curriculum</button>
